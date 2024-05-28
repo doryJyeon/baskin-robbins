0611c1b0a559e38540c029853ec708cd9e86e8d4
JYeon <doryJYeon@gmail.com>
2024-05-28 21:59:58 +0900

    fix_배라광장 menu active

--- src/pages/PlazaPage.tsx
+++ src/pages/PlazaPage.tsx
@@ -7,7 +7,7 @@ import { InnerContainer } from "../styles/GlobalStyle";
 const PlazaPage = () => {
   return (
     <InnerContainer top="160px">
-      <PlayLinks menuCheck={"event"} />
+      <PlayLinks menuCheck={"plaza"} />
 
       <TitleDesc title={"배라광장"} desc={"배라에게 묻고 배라가 답해드려요,<br />배라에게 다양한 의견을 전달해주세요"} />
 
