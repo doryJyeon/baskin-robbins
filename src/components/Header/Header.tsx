568d99a5c8b74b02d48a86ff464ec47af34689a2
JYeon <doryJYeon@gmail.com>
2024-05-31 22:44:31 +0900

    upd_header sub menu & search box

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,19 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom"
-import { HeaderContainer, HeaderWrapper, LogoImage, MenuWrapper, SearchWrapper, SignWrapper } from "./style"
-import { FaArrowRightFromBracket, FaUser } from "react-icons/fa6";
+import { HeaderContainer, HeaderWrapper, LogoImage, MenuDetailWrapper, MenuWrapper, SearchDiv, SearchWrapper, SignWrapper } from "./style"
+import { FaArrowRightFromBracket, FaRegCircleXmark, FaUser } from "react-icons/fa6";
+import { FaSearch } from "react-icons/fa";
 
 const Header = () => {
   const login = false;  // global
   const [scroll, setScroll] = useState(false);
+  const [search, setSearch] = useState(false);
+  const [menu, setMenu] = useState<[boolean, string]>([false, ""]);
 
   const isRoot = location.pathname === '/';
   const handleScroll = () => window.pageYOffset > 30 ? setScroll(true) : setScroll(false)
+
+  const searchInput = document.querySelector<HTMLInputElement>('input[name="searchText"]');
 
   useEffect(() => {
     !isRoot
@@ -20,35 +25,126 @@ const Header = () => {
     };
   }, [isRoot])
 
+  // menu hover
+  const handleMouseOver = (event: React.MouseEvent<HTMLAnchorElement>) => {
+    const menuPath = event.currentTarget.getAttribute('href')?.split("/")[1] || "";
+    setMenu([true, menuPath]);
+    setScroll(true);
+  };
+  const handleMouseLeave = () => {
+    setMenu([false, ""]);
+    setScroll(false);
+  };
+
+  // search show/hide
+  const handleFocusSearch = () => {
+    setMenu([false, ""]);
+    setScroll(true);
+    setSearch(true);
+
+    searchInput!.focus();
+  }
+  const handleBlurSearch = () => {
+    isRoot && setScroll(false);
+    setSearch(false);
+  }
+
+  // search enter key
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+      event.preventDefault(); // 기본 Enter 동작 방지
+      const submitButton = document.querySelector<HTMLButtonElement>('button.submit');
+      if (searchInput!.value.trim()) submitButton?.click();
+    }
+  };
 
   return (
-    <HeaderWrapper scroll={scroll}>
+    <HeaderWrapper show={scroll}>
       <HeaderContainer>
-        <Link to="/">
+        <Link to="/" onMouseOver={handleMouseLeave}>
           <LogoImage src={`/images/logos/${scroll ? "h_logo_2" : "h_logo"}.png`} alt="logo" />
         </Link>
 
-        <MenuWrapper scroll={scroll}>
-          <Link to={"/menu"}>Menu</Link>
-          <Link to={"/play/event/all"}>BR Play</Link>
-          <Link to={"/story/history"}>BR Story</Link>
+        <MenuWrapper show={scroll}>
+          <Link to={"/menu"} onMouseOver={handleMouseOver}>Menu</Link>
+          <Link to={"/play/event/all"} onMouseOver={handleMouseOver}>BR Play</Link>
+          <Link to={"/story/history"} onMouseOver={handleMouseOver}>BR Story</Link>
+          <Link to={"/store/dellvery"} onMouseOver={handleMouseOver}>Dellvery</Link>
         </MenuWrapper>
 
         <SearchWrapper
-          scroll={scroll}
-          type="text"
-          onFocus={() => setScroll(true)}
-          onBlur={() => setScroll(false)}
-          placeholder="제품명 검색"
+          show={scroll}
+          onClick={handleFocusSearch}
         />
 
-        <SignWrapper scroll={scroll}>
+        <SignWrapper show={scroll} onMouseOver={handleMouseLeave}>
           {login
             ? <Link to={"/signout"}><FaArrowRightFromBracket /></Link>
             : <Link to={"/signin"}><FaUser /></Link>
           }
         </SignWrapper>
       </HeaderContainer>
+
+      <MenuDetailWrapper show={menu[0]} menuType={menu[1]} onMouseLeave={handleMouseLeave}>
+        <li className="menu__menu">
+          <ol>
+            <li><Link to={"/menu/month"}>이달의 맛</Link></li>
+            <li><Link to={"/menu/ice_cream"}>아이스크림</Link></li>
+            <li><Link to={"/menu/prepack"}>프리팩</Link></li>
+            <li><Link to={"/menu/ice_cream_cake"}>아이스크림케이크</Link></li>
+            <li><Link to={"/menu/dessert"}>디저트</Link></li>
+            <li><Link to={"/menu/beverage"}>음료</Link></li>
+            <li><Link to={"/menu/coffee"}>커피</Link></li>
+          </ol>
+        </li>
+        <li className="menu__play">
+          <ol>
+            <li>
+              <Link to={"/play/event/all"}>이벤트</Link>
+              <p>
+                <Link to={"/play/event/promotion"}>프로모션</Link>
+                <Link to={"/play/event/benefit"}>제휴혜택</Link>
+              </p>
+            </li>
+            <li className="dashed_left">
+              <Link to={"/play/plaza"}>BR Play</Link>
+              <p>
+                <Link to={"/play/plaza"}>배라광장</Link>
+              </p>
+            </li>
+          </ol>
+        </li>
+        <li className="menu__story">
+          <ol>
+            <li><Link to={"story/history"}>이달의 맛 히스토리</Link></li>
+            <li><Link to={"story/be_better"}>Be Better</Link></li>
+          </ol>
+        </li>
+        <li className="menu__store">
+          <ol>
+            <li>
+              <Link to={"/store/dellvery"}>Dellvery</Link>
+              <p>
+                <Link to={"/store/dellvery"}>배달주문</Link>
+                <Link to={"/store/catering"}>단체주문</Link>
+              </p>
+            </li>
+          </ol>
+        </li>
+      </MenuDetailWrapper>
+
+      <SearchDiv show={search}>
+        <form action="/search" >
+          <input
+            type="text"
+            name="searchText"
+            placeholder="제품명을 입력하세요"
+            onKeyDown={handleKeyDown}
+          />
+          <button className="cancel" type="button" onClick={handleBlurSearch}><FaRegCircleXmark /></button>
+          <button className="submit" type="submit"><FaSearch /></button>
+        </form>
+      </SearchDiv>
     </HeaderWrapper>
   )
 }
--- src/components/Header/style.ts
+++ src/components/Header/style.ts
@@ -4,15 +4,19 @@ import { Container, colors } from "../../styles/GlobalStyle";
 const headerHeight = "90px";
 
 interface Props {
-  scroll: boolean;
+  show: boolean;
+}
+interface DetailProps {
+  show: boolean;
+  menuType: string;
 }
 
 export const HeaderWrapper = styled.header<Props>`
   width: 100%;
   height: ${headerHeight};
-  background-color: ${props => props.scroll ? colors.primary : "transparent"};
+  background-color: ${props => props.show ? colors.primary : "transparent"};
   position: fixed;
-  z-index: 500;
+  z-index: 100;
 `;
 
 export const HeaderContainer = styled(Container)`
@@ -31,14 +35,14 @@ export const LogoImage = styled.img`
 
 export const MenuWrapper = styled.div<Props>`
   > a {
-    color: ${props => props.scroll ? "#fff" : colors.dark};
-    font-size: 1.1rem;
+    color: ${props => props.show ? "#fff" : colors.dark};
+    font-size: 1rem;
     font-weight: 500;
     margin-right: 20px;
   }
 `;
 
-export const SearchWrapper = styled.input<Props>`
+export const SearchWrapper = styled.div<Props>`
   width: 180px;
   height: 1.5rem;
   margin-left: auto;
@@ -53,15 +57,10 @@ export const SearchWrapper = styled.input<Props>`
   background-image: url("/images/icons/btn_search.png");
   border-bottom: 2px solid ${colors.primary};
   color: ${colors.dark};
+  cursor: pointer;
   
-  ${props => props.scroll &&
+  ${props => props.show &&
     "border-bottom-color: #fff; background-image: url(/images/icons/btn_search_white.png);"
-  }
-
-  &::placeholder {
-    font-size: 1rem;
-    color: #33333350;
-    ${props => props.scroll && "color: #ffffff90;"}
   }
 `;
 
@@ -70,6 +69,121 @@ export const SignWrapper = styled.div<Props>`
   vertical-align: bottom;
 
   > a {
-    color: ${props => props.scroll ? "#fff" : colors.dark}
+    color: ${props => props.show ? "#fff" : colors.dark}
   }
 `;
+
+export const MenuDetailWrapper = styled.ul<DetailProps>`
+  width: calc(100% - 200px);
+  height: 172px;
+  padding: 0 100px;
+  background-color: #fff;
+  display: block;
+  box-shadow: 0px 4px 4px #00000020;
+  display: ${props => props.show ? "block" : "none"};
+
+  > li {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start;
+    cursor: pointer;
+    transition: all 0.2s linear;
+
+    > ol {
+      width: auto;
+
+      > li {
+        width: auto;
+        color: ${colors.dark};
+        margin-right: 38px;
+        font-size: 1.375rem;
+        font-weight: 500;
+        display: inline-block;
+        
+        > a {
+          transition: all 0.2s linear;
+        }
+
+        > p {
+          position: relative;
+          margin-top: 10px;
+
+          > a {
+            padding: 10px 10px 10px 0;
+            color: ${colors.font};
+            font-weight: 600;
+            font-size: 1rem;
+            margin-right: 38px;
+            transition: all 0.2s linear;
+          }
+        }
+        
+        &.dashed_left {
+          border-left: 1px dashed ${colors.font};
+          padding-left: 90px;
+        }
+        &:hover > a, & > p > a:hover {
+          color: ${colors.deep};
+          opacity: 1;
+        }
+      }
+    }
+
+    &.menu__menu {
+      display: ${props => props.menuType === "menu" ? "flex" : "none"};
+    }
+    &.menu__play {
+      display: ${props => props.menuType === "play" ? "flex" : "none"};
+    }
+    &.menu__story {
+      display: ${props => props.menuType === "story" ? "flex" : "none"};
+    }
+    &.menu__store {
+      display: ${props => props.menuType === "store" ? "flex" : "none"};
+    }
+  }
+`
+
+export const SearchDiv = styled.div<Props>`
+  width: 100%;
+  height: 120px;
+  background-color: #fff;
+  position: absolute;
+  top: ${props => props.show ? "0" : "100px"};
+  opacity: ${props => props.show ? "1" : "0"};
+  z-index: ${props => props.show ? "110" : "-1"};
+  transition: all .3s linear;
+
+  > form {
+    width: 100%;
+    max-width: 500px;
+    height: 2rem;
+    padding: 40px 0 8px;
+    margin: auto;
+    display: flex;
+    border-bottom: 2px solid ${colors.dark};
+
+    > input, > button {
+      outline: none;
+      border: none;
+      background: transparent;
+    }
+    > input {
+      width: 100%;
+      height: 100%;
+      font-size: 2rem;
+      text-align: center;
+      color: ${colors.dark};
+    }
+    > button {
+      width: 2rem;
+      height: 2rem;
+      font-size: 2rem;
+      margin-left: 1rem;
+      color: ${colors.dark};
+      cursor: pointer;
+    }
+  }
+`
