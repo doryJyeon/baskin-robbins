f86396979107781e2705aeb5a34c9b5082d18b7d
JYeon <doryJYeon@gmail.com>
2024-06-04 22:51:53 +0900

    UPD_data/footer menu link, DEL_미사용 data

--- src/data/FooterMenus.ts
+++ src/data/FooterMenus.ts
@@ -9,24 +9,24 @@ interface Menus {
 
 export const footerMenus: Menus = {
   "Menu": {
-    "이달의 맛": ["none", false],
-    "아이스크림": ["none", false],
-    "블록팩/레디팩": ["none", false],
-    "아이스크림 케이크": ["none", false],
-    "디저트": ["none", false],
-    "음료": ["none", false],
-    "커피": ["none", false],
+    "이달의 맛": ["menu/month", false],
+    "아이스크림": ["menu/ice_cream", false],
+    "블록팩/레디팩": ["menu/prepack", false],
+    "아이스크림 케이크": ["menu/ice_cream_cake", false],
+    "디저트": ["menu/dessert", false],
+    "음료": ["menu/beverage", false],
+    "커피": ["menu/coffee", false],
   },
   "BR Play": {
-    "이벤트": ["none", false],
-    "배라광장": ["none", false]
+    "이벤트": ["play/event/all", false],
+    "배라광장": ["play/plaza", false]
   },
   "BR Story": {
-    "이달의 맛 히스토리": ["none", false],
-    "Be Better": ["none", false]
+    "이달의 맛 히스토리": ["story/history", false],
+    "Be Better": ["story/be_better", false]
   },
   "Delivery/Store": {
-    "배달주문": ["none", false],
-    "단체주문": ["none", false]
+    "배달주문": ["store/delivery", false],
+    "단체주문": ["store/cetring", false]
   }
 }
